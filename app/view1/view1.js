4497a0c9c35b5e4f5fb724ca6a73ef30345a460e
markAking <mark@iliketocolor.com>
2016-08-11 14:47:24 -0400

    dashboard test ready

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,61 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+  $routeProvider.when('/', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
+.controller('View1Ctrl', ['$scope', 'AgeFactory', 'Funfactory', function($scope, AgeFactory, Funfactory) {
+    $scope.guest = {};
+    $scope.facts = false;
 
-.controller('View1Ctrl', [function() {
+    $scope.submit = function() {
+        $scope.guest.age = AgeFactory.getAge($scope.guest.dob);
+        Funfactory.getDateFact($scope.guest.dob).then(function(fact) {
+            $scope.guest.fact_date = fact;
+            Funfactory.getMathFact($scope.guest.age).then(function(fact) {
+                $scope.guest.fact_math = fact;
+                Funfactory.getTriviaFact($scope.guest.age).then(function(fact) {
+                    $scope.guest.fact_trivia = fact;
+                    $scope.facts = true;
+                });
+            });
+        });
+    }
 
+    $scope.reset = function() {
+        $scope.guest = {};
+        $scope.facts = false;
+    }
+}])
+.factory('AgeFactory', [function() {
+    return {
+        getAge: function(dob) {
+            var ageDate = new Date(Date.now() - dob.getTime());
+            return Math.abs(ageDate.getUTCFullYear() - 1970);
+        }
+    }
+}])
+.factory('Funfactory', ["$http", function($http) {
+    return{
+        getDateFact: function(dob) {
+            var month = dob.getMonth() + 1, 
+                day   = dob.getDate();
+
+            return $http.get('http://numbersapi.com/' + month + '/' + day + '/date').then(function(fact) {
+                return fact.data;
+            });  
+        },
+        getMathFact: function(age) {
+            return $http.get('http://numbersapi.com/' + age + '/math').then(function(fact) {
+                return fact.data;
+            });
+        },
+        getTriviaFact: function(age) {
+            return $http.get('http://numbersapi.com/' + age).then(function(fact) {
+                return fact.data;
+            });
+        }
+    }
 }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -2,15 +2,47 @@
 
 describe('myApp.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+    beforeEach(module('myApp.view1'));
+    var scope, view1Ctrl, AgeFactory, Funfactory;
 
-  describe('view1 controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        view1Ctrl = $controller('View1Ctrl', {
+            $scope: scope
+        });
+        AgeFactory = angular.injector(['myApp.view1']).get('AgeFactory');
+        Funfactory = angular.injector(['myApp.view1']).get('Funfactory');
     }));
+
+    describe('view1 controller', function(){
+
+        it('should be defined', inject(function($controller) {
+            expect(view1Ctrl).toBeDefined();
+        }));
+
+        it('should return the correct age from DOB', function() {
+            var dob = new Date("04/27/1999");
+            expect(AgeFactory.getAge(dob)).toEqual(17);
+        });
+
+        it('should return date fact', function() {
+            var dob = new Date("04/27/1999");
+            return Funfactory.getDateFact(dob).then(function(fact) {
+                expect(fact).toBeDefined();
+            });
+        });
+
+        it('should return math fact', function() {
+            return Funfactory.getMathFact(17).then(function(fact) {
+                expect(fact).toBeDefined();
+            });
+        });
+
+        it('should return trivia fact', function() {
+            return Funfactory.getTriviaFact(17).then(function(fact) {
+                expect(fact).toBeDefined();
+            });
+        });
 
   });
 });
